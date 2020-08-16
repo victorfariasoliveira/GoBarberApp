c56b83ce9718622d4275e02678d6775039ae28c5
Victor Farias <jose.victor.oliveira@accenture.com>
2020-08-16 19:03:39 -0300

    feature: adicionado signin e signup component e index de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './routes';
 
 /**
  * backgroundColor= "#312e38" => Serve apenas para o Android
@@ -8,13 +12,15 @@ import { View, StatusBar } from 'react-native';
 
 
 const App: React.FC = () => (
-  <>
+  <NavigationContainer>
   <StatusBar 
     barStyle="light-content"
     backgroundColor= "#312e38"
   />
-  <View style={{ flex: 1, backgroundColor: '#312e38' }} />
-  </>
+  <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+    <Routes />
+  </View>
+  </NavigationContainer>
 )
 
 export default App;

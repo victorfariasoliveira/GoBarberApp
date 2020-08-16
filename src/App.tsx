d1067bca3682011fa1ce1cf801f53086d1c5a883
Victor Farias <jose.victor.oliveira@accenture.com>
2020-08-16 18:16:33 -0300

    feature: adicionado estilizacao background e statusbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StatusBar } from 'react-native';
 
-const App: React.FC = () => <View />;
+/**
+ * backgroundColor= "#312e38" => Serve apenas para o Android
+ * style={{ flex: 1, backgroundColor: '#312e38' }} => toda a estilização se dar por meio dessa propriedade
+ */
+
+
+const App: React.FC = () => (
+  <>
+  <StatusBar 
+    barStyle="light-content"
+    backgroundColor= "#312e38"
+  />
+  <View style={{ flex: 1, backgroundColor: '#312e38' }} />
+  </>
+)
 
 export default App;

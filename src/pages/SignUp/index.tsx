d811b9b6681287d117cd60dedcffbab2067a42fc
Victor Farias <jose.victor.oliveira@accenture.com>
2020-08-18 18:44:41 -0300

    feature: adicionando navegação e estilização componente SignUp

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
-import { Container } from './styles'
+import { Image, View } from 'react-native'
+import logoImg from '../../assets/logo.png'
+
+import Input from '../../components/Input'
+import Button from '../../components/Button'
+
+import Icon from 'react-native-vector-icons/Feather'
+import { useNavigation } from '@react-navigation/native'
+
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles'
 
 const SignUp: React.FC = () => {
+  const navigation = useNavigation();
   return (
-    <Container />
+    <>
+    <Container>
+      <Image source={logoImg} />
+
+      <View>
+        <Title>Crie sua conta</Title>
+      </View>
+      
+      <Input name="name" icon="user" placeholder="Nome" placeholderColor="#666360"/>
+      <Input name="email" icon="mail" placeholder="E-mail" placeholderColor="#666360"/>
+      <Input name="password" icon="lock" placeholder="Senha" placeholderColor="#666360"/>
+      <Button onPress={() => { console.log('funfou') }}>Entrar</Button>
+
+    </Container>
+
+    <BackToSignIn onPress={() => navigation.navigate('SignIn')}>
+      <Icon name="arrow-left" size={20} color="#fff"/>
+      <BackToSignInText>Voltar para logon</BackToSignInText>
+    </BackToSignIn>
+    </>
   )
 }
 

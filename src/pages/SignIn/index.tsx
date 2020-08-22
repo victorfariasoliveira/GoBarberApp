2e28d0fe418ad8fef055e77f2afc4235fe7a8877
Victor Farias <jose.victor.oliveira@accenture.com>
2020-08-22 16:05:06 -0300

    feature: adicionado unform e integração com formuários em SignIn e SignUp

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { Image, View } from 'react-native'
 import logoImg from '../../assets/logo.png'
 
@@ -8,10 +8,18 @@ import Button from '../../components/Button'
 import Icon from 'react-native-vector-icons/Feather'
 import { useNavigation } from '@react-navigation/native'
 
+import { Form } from '@unform/mobile'
+import { FormHandles } from '@unform/core'
+
 import { Container, Title, ForgotPassword, ForgotPasswordButton, CreateAccountButton, CreateAccountButtonText } from './styles'
 
 const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
   const navigation = useNavigation();
+
+  const handleSignIn = useCallback((data: object) => {
+    console.log(data)
+  }, [])
   return (
     <>
     <Container>
@@ -21,9 +29,11 @@ const SignIn: React.FC = () => {
         <Title>Faça seu logon</Title>
       </View>
 
-      <Input name="email" icon="mail" placeholder="E-mail" placeholderColor="#666360"/>
-      <Input name="password" icon="lock" placeholder="Senha" placeholderColor="#666360"/>
-      <Button onPress={() => { console.log('funfou') }}>Entrar</Button>
+      <Form ref={formRef} onSubmit={handleSignIn}>
+        <Input name="email" icon="mail" placeholder="E-mail" placeholderColor="#666360"/>
+        <Input name="password" icon="lock" placeholder="Senha" placeholderColor="#666360"/>
+        <Button onPress={() => { formRef.current?.submitForm() }}>Entrar</Button>
+      </Form>
 
       <ForgotPassword onPress={() => {}}>
         <ForgotPasswordButton>Esqueci minha senha</ForgotPasswordButton>
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { Image, View } from 'react-native'
 import logoImg from '../../assets/logo.png'
 
@@ -8,10 +8,19 @@ import Button from '../../components/Button'
 import Icon from 'react-native-vector-icons/Feather'
 import { useNavigation } from '@react-navigation/native'
 
+import { Form } from '@unform/mobile'
+import { FormHandles } from '@unform/core'
+
 import { Container, Title, BackToSignIn, BackToSignInText } from './styles'
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
   const navigation = useNavigation();
+
+  const handleSignUp = useCallback((data: object) => {
+    console.log(data)
+  }, [])
+
   return (
     <>
     <Container>
@@ -20,11 +29,12 @@ const SignUp: React.FC = () => {
       <View>
         <Title>Crie sua conta</Title>
       </View>
-      
-      <Input name="name" icon="user" placeholder="Nome" placeholderColor="#666360"/>
-      <Input name="email" icon="mail" placeholder="E-mail" placeholderColor="#666360"/>
-      <Input name="password" icon="lock" placeholder="Senha" placeholderColor="#666360"/>
-      <Button onPress={() => { console.log('funfou') }}>Entrar</Button>
+      <Form ref={formRef} onSubmit={handleSignUp}>
+        <Input name="name" icon="user" placeholder="Nome" placeholderColor="#666360"/>
+        <Input name="email" icon="mail" placeholder="E-mail" placeholderColor="#666360"/>
+        <Input name="password" icon="lock" placeholder="Senha" placeholderColor="#666360"/>
+        <Button onPress={() =>  formRef.current?.submitForm() }>Entrar</Button>
+      </Form>
 
     </Container>
 

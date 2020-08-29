3e1b06cdfbe1cf9e824e7ab0a902cda1abbad361
Victor Farias <jose.victor.oliveira@accenture.com>
2020-08-29 20:27:46 -0300

    feature: adicionado validação do form com Yup

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useImperativeHandle, forwardRef } from 'react';
+import React, { useEffect, useRef, useState, useCallback, useImperativeHandle, forwardRef } from 'react';
 import { TextInputProps } from 'react-native'
 import { useField } from '@unform/core'
 
@@ -26,10 +26,23 @@ const Input: React.RefForwardingComponent<InputRef ,InputProps> = ({ name, icon,
   const { registerField, defaultValue = '', fieldName, error } = useField(name);
   const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
 
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true)
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false)
+
+    setIsFilled(!! inputValueRef.current.value) //true or false
+  }, [])
+
   useImperativeHandle(ref, () => ({ 
     focus() {
       inputElementRef.current.focus()}
-    }))
+    }));
 
   useEffect(() => {
     registerField<string>({
@@ -48,13 +61,15 @@ const Input: React.RefForwardingComponent<InputRef ,InputProps> = ({ name, icon,
   }, [fieldName, registerField])
 
   return (
-    <Container>
-      <Icon name={icon} size={20} color="#666360"/>
+    <Container isFocused={isFocused}>
+      <Icon name={icon} size={20} color={isFocused || isFilled ? '#ff9000' : '#666360'}/>
       <TextInput 
         ref={inputElementRef}
         keyboardAppearance="dark"
         placeholderTextColor={placeholderColor}
         defaultValue={defaultValue}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
         onChangeText={(value) => {inputValueRef.current.value = value}} 
         {...rest}
         />
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,11 @@
 import React, { useCallback, useRef } from 'react';
-import { Image, View, TextInput } from 'react-native'
+import { Image, View, TextInput, Alert } from 'react-native'
+import * as Yup from 'yup'
 import logoImg from '../../assets/logo.png'
 
 import Input from '../../components/Input'
 import Button from '../../components/Button'
+import getValidationErrors from '../../utils/getValidationError'
 
 import Icon from 'react-native-vector-icons/Feather'
 import { useNavigation } from '@react-navigation/native'
@@ -11,17 +13,61 @@ import { useNavigation } from '@react-navigation/native'
 import { Form } from '@unform/mobile'
 import { FormHandles } from '@unform/core'
 
-import { Container, Title, ForgotPassword, ForgotPasswordButton, CreateAccountButton, CreateAccountButtonText } from './styles'
+import { 
+  Container, 
+  Title, 
+  ForgotPassword, 
+  ForgotPasswordButton, 
+  CreateAccountButton, 
+  CreateAccountButtonText 
+  } from './styles'
+
+interface SignInFormData {
+  email: string,
+  password: string
+}
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const passswordInputRef = useRef<TextInput>(null);
-
   const navigation = useNavigation();
 
-  const handleSignIn = useCallback((data: object) => {
-    console.log(data)
-  }, [])
+  const handleSignIn = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail é obrigatório')
+            .email('Digite um e-mail válido'),
+          password: Yup.string().required('Senha é obrigatória'),
+        });
+
+        await schema.validate(data, { abortEarly: false });
+        /*
+        await signIn({
+          email: data.email,
+          password: data.password,
+        });
+        */
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+          return;
+        }
+
+        Alert.alert(
+          'Erro na autenticação',
+          'Ocorreu um error ao fazer login, cheque as credenciais.',
+        );
+      }
+    },
+    [],
+  );
+
   return (
     <>
     <Container>
